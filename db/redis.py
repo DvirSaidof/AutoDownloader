d6a119dbf8d3b1cdf5f7eb1db00e4822b405cdf1
DvirSaidof <34963960+DvirSaidof@users.noreply.github.com>
2023-01-28 19:05:25 +0200

    Flask stuff

---
+++ db/redis.py
@@ -0,0 +1,47 @@
+import redis
+
+from typing import List
+
+
+class RedisClient:
+
+    MAX_RECENT_DLS_LEN = 10
+    DB_NAME = "recent_downloads"
+
+    def __init__(self, host, port):
+        self.__redis = redis.Redis(host=host, port=port)
+        recent_downloads_undecoded = self.__redis.lrange(self.DB_NAME, 0, -1)
+        self.recent_downloads = [recent_download.decode("utf-8") for recent_download in recent_downloads_undecoded]
+
+    def __save_db(self):
+        with self.__redis.pipeline() as pipe:
+            pipe.delete(self.DB_NAME)
+            pipe.rpush(self.DB_NAME, *self.recent_downloads)
+            pipe.execute()
+
+    def get_recent_downloads(self) -> List:
+        return self.recent_downloads
+
+    def add_recent_download(self, movie: str):
+
+        if not self.verify_movie_not_in_recent_downloads(movie):
+            return False
+
+        if len(self.recent_downloads) == self.MAX_RECENT_DLS_LEN:
+            self.recent_downloads.pop()
+
+        self.recent_downloads.insert(0, movie)
+
+        self.__save_db()
+        with self.__redis.pipeline() as pipe:
+            pipe.delete(self.DB_NAME)
+            pipe.rpush(self.DB_NAME, *self.recent_downloads)
+            pipe.execute()
+
+        return True
+
+    def verify_movie_not_in_recent_downloads(self, movie: str):
+        if movie in self.recent_downloads:
+            return False
+        return True
+
--- gui/gui.py
+++ gui/gui.py
@@ -1,101 +1,171 @@
-# from flask import Flask, render_template, request
-#
-# #
-# # @app.route('/', methods=["POST"])
-# # def __main__():
-# #     if request.method == "POST":
-# #         movie_name = request.form.get("movie_name")
-# #         movie_year = request.form.get("movie_year")
-# #         print(f"Movie: {movie_name} {movie_year}")
-# #         return f"Movie: {movie_name} {movie_year}"
-# #     return render_template("search_movie.jinja2")
-#
-#
-# class DownloaderGUI:
-#     app = None
-#
-#     def __init__(self):
-#         self.app = Flask(__name__)
-#         self.app.run(debug=True, host="0.0.0.0", port=80)
-#         self.app.add_url_rule('/', '', self.my_form)
-#         self.film_name = None
-#         self.film_year = None
-#
-#     #@app.route('/')
-#     def my_form(self):
-#         return render_template('search_movie.jinja2')
-#
-#     def index(self):
-#         pass
-#
-#     #@app.route('/', methods=['POST'])
-#     def my_form_post(self):
-#         if request.method == "POST":
-#             self.film_name = request.form.get("movie_name")
-#             self.film_year = request.form.get("movie_year")
-#             #GO TO RESULTS PAGE
-#
-#     @property
-#     def get_film_name(self):
-#         return self.film_name
-#
-#     @property
-#     def get_film_year(self):
-#         return self.film_year
-#
-#
-# # if __name__ == "__main__":
-# #     app.run(debug=True, host="0.0.0.0", port=80)
-# #
+import os
+import logging
+import webbrowser
+import time
+import json
+from celery import Celery
+from typing import List, Dict
+from db.redis import RedisClient
+from pages.subtitles import Subtitles, SubtitlesNotFoundException, DestinationFolderNotFoundException
 from flask import Flask, render_template, request, redirect, url_for
+from pages.pb_torrents_page import SearchTorrentPirateBay
+from parsers.pb_torrent_parser import PirateBayFilmTorrent
+
+
+logging.basicConfig(format='%(asctime)s %(levelname)-8s [%(filename)s:%(lineno)d] %(message)s',
+                    datefmt='%d-%m-%Y:%H:%M:%S', level=logging.DEBUG, filename='auto_download_app.log')
+
+logging.FileHandler('auto_download_app_gui.log', 'w', 'utf-8')
+
 
 app = Flask(__name__)
 
 
-#This will be torrent list
-current_search = {
-    'film_name': 'Titanic',
-    'film_year': '1997'
-}
+class AutoDownloadGUI:
+    """
+    The AutoDownloadApp interacts with the user/google doc files and receive a file_name to download.
+    It will then perform a search query on PirateBay using SearchTorrentPirateBay Class, and download the best result
+    (E.G torrent with most seeders).
+    Download the torrent using webbroswer module to open UTorrent.
+    Download subtitles with Subtitles Class which gets language preferences
+    Place all files in a shared folder on a local or remote machine.
+    """
+
+    MAX_NUM_OF_TORRENTS = 20
+    SLEEP_TIME_AFTER_DL_START = 30
+    GOOGLE_SHEET_UI_SLEEP = 3
+
+    def __init__(self, config):
+        #cur_path = os.path.dirname(__file__)
+        #new_path = os.path.relpath(config, cur_path)
+        #print(new_path)
+        with open(config, 'r') as f:
+            self.config = json.loads(f.read())
+
+        self.film_name = None
+        self.torrents_options = dict()
+        self.torrents = []
+        self.torrent_selected = None
+        self.opensubtitles_key = self.config.get('opensubtitles_credentials').get('api_key')
+        self.folder = self.config.get('user_preferences').get('folder')
+        self.language_preferences = self.config.get('user_preferences').get('language')
+        self.logger = logging.getLogger("gui")
+
+
+        #self.redis_db = redis.Redis(host='localhost', port=6379)
+        self.redis_client = RedisClient(host='localhost', port=6379)
+        self.torrents = []
+        #self.redis_client.get_torrents()
+        #self.redis_db.mset({"": "", "": ""})
+
+    def search_movie(self, film_name):
+        query = SearchTorrentPirateBay()
+        torrents = query.search_film(film_name)
+        if not torrents:
+            self.logger.debug("Couldn't find any torrent!")
+        else:
+            self.torrents = torrents[:self.MAX_NUM_OF_TORRENTS - 1]
+
+    def download_movie(self, torrent: PirateBayFilmTorrent):
+        print(f"The following torrent will be downloaded: {torrent}")
+        self.logger.debug(f"The following torrent will be downloaded: {torrent}")
+        webbrowser.open(torrent.magnet_link)
+
+    def download_subtitles(self):
+        subs = Subtitles(self.opensubtitles_key)
+        year = str(self.torrent_selected.year) if self.torrent_selected.year else None
+        for lang in self.language_preferences:
+            try:
+                subs_file_id = subs.search_subs(lang=lang,
+                                                year=year,
+                                                resolution=self.torrent_selected.resolution,
+                                                quality=self.torrent_selected.quality,
+                                                codec=self.torrent_selected.codec,
+                                                title=self.torrent_selected.movie_name,
+                                                group=self.torrent_selected.group,
+                                                excess=self.torrent_selected.excess,
+                                                )
+                subs.download_subs(lang=lang,
+                                   file_name=self.torrent_selected.torrent_name,
+                                   film_name_short=self.film_name,
+                                   file_id=subs_file_id,
+                                   base_folder=self.folder)
+            except SubtitlesNotFoundException as e:
+                print(e)
+                self.logger.error(e)
+            except DestinationFolderNotFoundException as e:
+                print(e)
+                self.logger.error(e)
+            except Exception as e:
+                print(e)
+                self.logger.error(e)
 
 
 @app.route("/")
 def home():
-    return render_template('home.jinja2', current_search=current_search)
+    return render_template('home.jinja2', recent_downloads=auto_gui.redis_client.get_recent_downloads())
 
 
-@app.route('/post/<int:post_id>')
-def torrent(post_id):
-    print(post_id)
-    return render_template('torrent.jinja2', current_search=current_search)
+@app.route('/post/<torrents>')
+def torrent(torrents):
+    print(torrents)
+    return render_template('torrent.jinja2', torrents=torrents)
 
 
 # args: 127.0.0.1:5000/post/create?title=blalala&content=something_else
 # form: 127.0.0.1:5000/post/create?title=blalala&content=something_else
-@app.route('/post/create', methods=['GET', 'POST'])
-def create():
+@app.route('/post/search', methods=['GET', 'POST'])
+def search():
     if request.method == 'POST':
         film_name = request.form.get('film_name')
         film_year = request.form.get('film_year')
-        current_search['film_name'] = film_name
-        current_search['film_year'] = film_year
-        return redirect(url_for('torrent', post_id=0))
+        auto_gui.film_name = film_name
+        auto_gui.search_movie(film_name)
+        if auto_gui.torrents and len(auto_gui.torrents) > 0:
+            return redirect(url_for('torrents_results'))
+        else:
+            msg = f"No torrent was found for: {auto_gui.film_name}..."
+            print(msg)
+            auto_gui.logger.debug(msg)
+            return render_template('error_empty_results.jinja2', film_name=auto_gui.film_name)
     return render_template('search_movie.jinja2')
 
 
-@property
-def get_film_name():
-    return current_search.get('film_name')
+@app.route('/post/results', methods=['GET', 'POST'])
+def torrents_results():
+    if request.method == 'POST':
+        selected_torrent_index = int(request.form['submit_button']) - 1
+        auto_gui.torrent_selected = auto_gui.torrents[selected_torrent_index]
+        auto_gui.logger.debug(f"Downloading {auto_gui.torrent_selected.torrent_name}")
+        ## Check if Torrent selected is in recent downloads.
+
+        is_added = auto_gui.redis_client.add_recent_download(auto_gui.torrent_selected.torrent_name)
+        if not is_added:
+            msg = f"Torrent was already downloaded: {auto_gui.torrent_selected.torrent_name}..."
+            print(msg)
+            auto_gui.logger.debug(msg)
+            return render_template('torrent_already_exists.jinja2', torrent=auto_gui.torrent_selected.torrent_name)
+
+        auto_gui.download_movie(auto_gui.torrent_selected)
 
 
-@property
-def get_film_year():
-    return current_search.get('film_year')
+        msg = f"Downloading {auto_gui.torrent_selected}..."
+        print(msg)
+        auto_gui.logger.debug(msg)
+        time.sleep(auto_gui.SLEEP_TIME_AFTER_DL_START)
+
+        auto_gui.download_subtitles()
+    return render_template('torrents_results.jinja2', torrents=auto_gui.torrents)
 
 
-def run():
-    app.run(debug=True)
+def run(config_file):
+    print("1")
+    global auto_gui
+    auto_gui = AutoDownloadGUI(config_file)
+    app.run(host="0.0.0.0", debug=True)
 
 
 if __name__ == '__main__':
+    print("1")
+    auto_gui = AutoDownloadGUI("../config/config.json")
     run()
