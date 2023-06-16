f48047f8093dcfeed334aa89d4c24a4f75520850
dvirsaidof <dvirsaidof@dvirs-MacBook-Pro.local>
2023-06-16 13:09:09 +0300

    some changes

--- subtitles_api/subtitles_tests.py
+++ subtitles_api/subtitles_tests.py
@@ -4,7 +4,7 @@ from subtitles_api.subtitles import Subtitles, SubtitlesNotFoundException, Desti
 
 class SubtitlesTests(unittest.TestCase):
 
-    def test_api_key_correct(self):
+    def test_api_wrong_key_error(self):
         wrong_api_keys = [None, 10, ""]
         for i, wrong_api_key in enumerate(wrong_api_keys):
             with self.subTest(test_number=f"Test # {i}"):
