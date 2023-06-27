23103a9d8d8619d61ad2bf04de937237d6d86f4b
dvirsaidof <dvirsaidof@dvirs-MacBook-Pro.local>
2023-06-27 23:33:17 +0300

    some changes

--- auto_download.sh
+++ auto_download.sh
@@ -1,4 +1,3 @@
-
 #function install_app() {
 #  shell_type="$1"
 #  app="$2"
@@ -27,14 +26,28 @@ function install_app() {
     echo "$app is not installed. Installing $app" >&2
     if [[ $shell_type == "zsh" ]]; then
       echo "Installing with brew"
-      sudo -u "$user_name" brew install $app
+      yes | sudo -u "$user_name" brew install $app
     else
       echo "Installing with apt"
-      apt install $app
+      yes | apt install $app
     fi
   else
     echo "$app is already installed"
   fi
+}
+function set_python_env() {
+  python_cmd="python"
+  pip_cmd="pip"
+  if ! type "$python_cmd" &> /dev/null; then
+    if type python3 &> /dev/null; then
+      python_cmd="python3"
+      pip_cmd="pip3"
+    else
+      echo "Couldn't find python installed in the system"
+      exit
+    fi
+  fi
+  echo "$python_cmd" "$pip_cmd"
 }
 
 user_name="$1"
@@ -42,26 +55,24 @@ shell_type="$(bash --version)"
 port="5000"
 config_path="$(pwd)/config/config.json"
 
+read python_cmd pip_cmd < <(set_python_env)
+
 echo "Installing python requirements"
-pip install -r requirements.txt
+"$pip_cmd" install -r requirements.txt
 
 if [[ $shell_type =~ "apple" ]]; then
   echo "Using zsh"
   install_app "zsh" "redis-server" $user_name
+  ip_address="$(ipconfig getifaddr en0)"
 else
   echo "Using bash"
   install_app "bash" "redis-server" $user_name
+  ip_address="$(hostname -I)"
 fi
 
 echo "Starting redis-server"
 redis-server &
 
-if [[ $shell_type =~ "apple" ]]; then
-  ip_address="$(ipconfig getifaddr en0)"
-else
-  ip_address="$(ipconfig getifaddr en0)"
-fi
-
-echo "Starting AutoDownloader App"
-python app.py "$config_path" "$port" "$ip_address"
+echo "Starting AutoDownloader App $ip_address:$port"
+"$python_cmd" app.py "$config_path" "$port" "$ip_address"
 
